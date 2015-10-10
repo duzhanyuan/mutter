ee8f4f705d08f3901913cfc7a03e4fa4d2064ae9
DuncanSamson <duncancsamson@gmail.com>
2015-10-10 15:58:31 +0100

    Add modal

--- html/js/actions.js
+++ html/js/actions.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
   $('#groups-add').click(function() {
-    alert('Hello Duncan');
+    $('#myModal').on('shown.bs.modal', function () {
+      $("#myModalLabel").text("Add Group")
+      $("#modal-body").html('<div class="form-group"><label for="group-name">Group Name</label><input type="text" class="form-control" id="group-name" placeholder="Group Name"></div>')
+    })
+
+    $('#myModal').on('hide.bs.modal', function () {
+      console.log("Add group closed.")
+    })
+
+    $('#myModal #action-confirm').click(function (){
+      console.log("Add group confirmed")
+      console.log($('#group-name').val())
+
+      $('#myModal').modal('hide')
+    })
   })
 });
